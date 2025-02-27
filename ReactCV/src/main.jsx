ed0e0eb8773b6c77c39393a3464d7300a2acec9c
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-02-27 21:39:44 +0100

    Fixing the bug in the main.jsx file

--- ReactCV/src/main.jsx
+++ ReactCV/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename="/REACTCV-WEBSITE">
+  <BrowserRouter basename="/ReactCV">
     <App />
   </BrowserRouter>
 );
