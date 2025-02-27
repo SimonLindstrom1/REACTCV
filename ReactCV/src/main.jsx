3bb24ca865daa980775bfd9d2fb2daa2f3da216e
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-02-27 23:39:29 +0100

    yes

--- ReactCV/src/main.jsx
+++ ReactCV/src/main.jsx
@@ -1,13 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <BrowserRouter basename="/ReactCV">
+createRoot(document.getElementById('root')).render(
     <App />
-  </BrowserRouter>
-);
+)
