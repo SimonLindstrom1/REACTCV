3c6df95941c705ef529641fda9d5cc4abc0e6fdb
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-02-28 16:12:29 +0100

    s

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/REACTCV/", // Update this to match your GitHub repository name
+  base: "/REACTCV/",
+  plugins: [react()]
 });
