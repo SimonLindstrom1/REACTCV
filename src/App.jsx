a9de064433e40ac7605f5017c16af87e3ded1019
Simon Lindström <simon.tiger.lindstrom@gmail.com>
2025-02-28 15:26:57 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 import Career from './pages/Career';
 import Portfolio from './pages/Portfolio';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Navbar />
@@ -19,8 +19,9 @@ function App() {
         <Route path="/career" element={<Career />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
+      
     </div>
   );
-}
+};
 
 export default App;
